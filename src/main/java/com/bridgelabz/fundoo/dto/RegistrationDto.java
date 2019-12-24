6884d5e553940b228745da592da7c1e9ca7304c1
tushar <tusharnayak1996@gmail.com>
2019-12-24 10:27:55 +0530

    modify:using lombok

--- src/main/java/com/bridgelabz/fundoo/dto/RegistrationDto.java
+++ src/main/java/com/bridgelabz/fundoo/dto/RegistrationDto.java
@@ -1,7 +1,11 @@
 package com.bridgelabz.fundoo.dto;
 
 import javax.validation.constraints.NotBlank;
+
 import javax.validation.constraints.Pattern;
+
+import lombok.Data;
+@Data
 public class RegistrationDto {
 	@NotBlank
 	private String firstName;
@@ -17,40 +21,5 @@ public class RegistrationDto {
 	private String confirmPassword;
 	
 	
-	public String getConfirmPassword() {
-		return confirmPassword;
-	}
-	public void setConfirmPassword(String confirmPassword) {
-		this.confirmPassword = confirmPassword;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+	
 }
--- src/main/java/com/bridgelabz/fundoo/dto/ResetPasswordDto.java
+++ src/main/java/com/bridgelabz/fundoo/dto/ResetPasswordDto.java
@@ -2,26 +2,13 @@ package com.bridgelabz.fundoo.dto;
 
 import javax.validation.constraints.NotBlank;
 
+import lombok.Data;
+@Data
 public class ResetPasswordDto {
 	@NotBlank
 	private String newPassword;
 	@NotBlank
 	private String confirmPassword;
 
-	public String getNewPassword() {
-		return newPassword;
-	}
-
-	public void setNewPassword(String newPassword) {
-		this.newPassword = newPassword;
-	}
-
-	public String getConfirmPassword() {
-		return confirmPassword;
-	}
-
-	public void setConfirmPassword(String confirmPassword) {
-		this.confirmPassword = confirmPassword;
-	}
-
+	
 }
